4b7de293314f9ca4bba210f445470490e41b2256
Jonathan Wood <bamblehorse@gmail.com>
2016-11-09 13:47:32 +0000

    changed back to old code basic left

--- views/js/main.js
+++ views/js/main.js
@@ -508,7 +508,7 @@ function updatePositions() {
 
   for (var i = 0; i < length; i++) {
     var phase = Math.sin(scroll + (i % 5));
-    items[i].style = "transform: translateX(" + (100 * phase) + "px)";
+    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
